60aa7dba10da474ff95349919d201083d1f39266
Alabi Temitope <t.banji@rocketmail.com>
2022-11-13 04:20:49 +0100

    root navigation created

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 // import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+
+const Stack = createStackNavigator();
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+   ...DefaultTheme.colors,
+   background: "transparent",
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      {/* <StatusBar style="auto" /> */}
-    </View>
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator>
+        <Stack.Screen />
+        <Stack.Screen />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
