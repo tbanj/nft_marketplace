53f3d0efab30326499a0f7952fd4ae556bb7d623
Alabi Temitope <t.banji@rocketmail.com>
2022-11-13 04:48:17 +0100

    implemented assets

--- App.js
+++ App.js
@@ -19,6 +19,17 @@ const theme = {
 
 
 function App() {
+
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+
+  });
+
+  if (!loaded) retu null;
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator screenOptions={{ headerShown: false}}
