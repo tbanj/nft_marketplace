8b715657207262bbad8b9808cd7faeee6cd3b4c4
Alabi Temitope <t.banji@rocketmail.com>
2022-11-13 05:01:31 +0100

    implemented constants

---
+++ components/FocusedStatusBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View } from 'react-native'
+import { Text } from 'react-native-web'
+
+ const FocusedStatusBar = () => {
+  return (
+    <View>
+        <Text>FocusedStatusBar</Text>
+    </View>
+  )
+}
+
+export default FocusedStatusBar
+
--- screens/Home.js
+++ screens/Home.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { Text } from 'react-native';
 import { View, SafeArea, FlatList, Text } from 'react-native';
 
+import { COLORS, NFTData} from '../constants';
 export const Home = () => {
   return (
-    <Text>Home</Text>
+    // <Text>Home</Text>
+    <SafeAreaView style={{flex: 1}}>
+
+    </SafeAreaView>
   )
 }
